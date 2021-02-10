6ec91726d77402adaa5f78acebc28c29e533553b
Dagur Valberg Johannsson <dagurval@pvv.ntnu.no>
2021-02-10 13:26:23 +0100

    rename header

--- generate-c-header.sh
+++ generate-c-header.sh
@@ -8,7 +8,7 @@ fi
 
 bindgen_options="--lang c --crate fujisaki_ringsig"
 
-cmd="cbindgen ${bindgen_options} --output c_header.h"
+cmd="cbindgen ${bindgen_options} --output fujisaki_ringsig.h"
 echo ${cmd}
 ${cmd}
 
